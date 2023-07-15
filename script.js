7ea50e3f1fca4b399f21ee39d904e045e9c61b2f
JairoMatos <jairo.oliveira22@gmail.com>
2023-07-15 18:07:35 -0300

    correcao

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const numIn = document.querySelector('#inicio');
 const numEnd = document.querySelector('#fim');
 const numStep = document.querySelector('#passo');
 const read = document.querySelector('.txtContador');
+
 const count = () => {
     let start = Number(numIn.value)
     let end = Number(numEnd.value)
@@ -11,8 +12,13 @@ const count = () => {
         while(start <= end){
             read.innerHTML += `${start}...`;
             start = start + steps;
+            numEnd.value = '';
+            numIn.value = '';
+            numStep.value = '';
         }
         read.innerHTML += ` 🏁`
     }
-    
+}
+function clean(){
+    read.innerHTML = '';
 }
